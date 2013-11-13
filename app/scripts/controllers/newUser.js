da6dbb6fd30757e27c098f53fc5f58c6a92c8294
Xavier Lagunas Calpe <xlagunas@gmail.com>
2013-11-13 23:53:52 +0100

    Added password checker when adding new user

--- app/scripts/controllers/newUser.js
+++ app/scripts/controllers/newUser.js
@@ -11,20 +11,23 @@ angular.module('video2browserApp')
     ];
     $scope.submit = function(person){
         $log.info(person);
-        $http({'method': 'PUT', 'url': 'http://localhost:8080/v2b/users/create', 'data': JSON.stringify(person)}).
-            success(function(data, status, headers, config) {
-                $log.debug("entra al success" +status)
-                $state.go("addPhoto");
-                // this callback will be called asynchronously
-                // when the response is available
-            }).
-            error(function(data, status, headers, config) {
-                // called asynchronously if an error occurs
-                // or server returns response with an error status.
-                $log.debug("entra al error "+status)
-                if (status === 400){
-                    $log.debug("errorType: "+data.errorType+"\nerrorDesc: "+data.description);
-                }
-            });
+        if ($scope.passConfirmation === person.password){
+            $http({'method': 'PUT', 'url': 'http://localhost:8080/v2b/users/create', 'data': JSON.stringify(person)}).
+                success(function(data, status, headers, config) {
+                    $log.debug("entra al success" +status)
+                    $state.go("addPhoto");
+                    // this callback will be called asynchronously
+                    // when the response is available
+                }).
+                error(function(data, status, headers, config) {
+                    // called asynchronously if an error occurs
+                    // or server returns response with an error status.
+                    $log.debug("entra al error "+status)
+                    if (status === 400){
+                        $log.debug("errorType: "+data.errorType+"\nerrorDesc: "+data.description);
+                    }
+                });
+        }
+
     }
   });

3f3547db6fb49b008c80550ca81472dc8daa53be
Xavier Lagunas Calpe <xlagunas@gmail.com>
2013-11-28 11:33:03 +0100

    Added password checker when adding new user

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -17,5 +17,26 @@ angular.module('video2browserApp', ['ui.router'])
         $http.get('http://localhost:8080/v2b/users/list').success(function(data){
             User.contacts = data;
             $log.info(data);
+
+
+            var protocols = ['websocket', 'xdr-streaming', 'xhr-streaming', 'iframe-eventsource', 'iframe-htmlfile',
+                'xdr-polling', 'xhr-polling', 'iframe-xhr-polling', 'jsonp-polling' ];
+
+            //var sock = new SockJS(document.URL+"/socket/", undefined, {protocols_whitelist: protocols, debug: true });
+            var sock = new SockJS("http://localhost:8080/v2b/socket/", undefined, {protocols_whitelist: protocols, debug: true });
+            sock.onopen = function(){console.log("obert desde angular");};
+
+//            sock.onmessage = function(e){
+//                console.log("message des d'angular: ",e.data);
+//                msg = JSON.parse(e.data);
+//                if (msg.messageType == "ADD_USER"){
+//                    ContactService.addContact({"idFriendship":5,"name":"TEST Lagunas Calpe","username":"clagunas","image":"resources/img/default_avatar.png","friendshipStatus":"PENDING","status":"OFFLINE"});
+//                }
+//
+//            };
+
+            sock.onclose = function(){
+                console.log("tancat");
+            };
         });
     });
--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -2,11 +2,33 @@
 
 angular.module('video2browserApp')
   .controller('LoginCtrl', function ($scope, $log, $http, $state) {
-    $http.defaults.use
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
 
-  });
+
+$scope.submit = function(login){
+    var queryString = "username="+login.username+"&password="+login.password;
+    $log.info(login);
+        $http({'method': 'POST', 'url': 'http://localhost:8080/v2b/login',
+            'headers': {'Content-Type': 'application/x-www-form-urlencoded'},
+            'data': queryString}).
+            success(function(data, status, headers, config) {
+                $log.debug("entra al success" +status)
+                $state.go("addPhoto");
+                // this callback will be called asynchronously
+                // when the response is available
+            }).
+            error(function(data, status, headers, config) {
+                // called asynchronously if an error occurs
+                // or server returns response with an error status.
+                $log.debug("entra al error "+status)
+                if (status === 400){
+                    $log.debug("errorType: "+data.errorType+"\nerrorDesc: "+data.description);
+                }
+            });
+    }
+
+$scope.checkUsername = function(username){
+    $log.info("checking" +username);
+}
+
+});
+
